56ea1aafeeba6defc6704cf92088c84696084f3c
Daniel Lee <lee@isi-solutions.org>
2014-05-01 17:23:03 +0200

    Update UI to allow usage of new classes

--- tune_calib_pair.py
+++ tune_calib_pair.py
@@ -93,6 +93,10 @@ class BMTuner(object):
         self.block_matcher = block_matcher
         #: Shortest dimension of image
         self.shortest_dimension = min(self.pair[0].shape[:2])
+        #: Settings chosen for ``BlockMatcher``
+        self.bm_settings = {}
+        for parameter in self.block_matcher.parameter_maxima.keys():
+            self.bm_settings[parameter] = []
         cv2.namedWindow(self.window_name)
         self._initialize_trackbars()
         self.tune_pair(image_pair)
@@ -115,6 +119,11 @@ class BMTuner(object):
         except BadBlockMatcherArgument:
             return
         self.update_disparity_map()
+    def _save_bm_state(self):
+        """Save current state of ``block_matcher``."""
+        for parameter in self.block_matcher.parameter_maxima.keys():
+            self.bm_settings[parameter].append(
+                                   self.block_matcher.__getattribute__(parameter))
     def update_disparity_map(self):
         """
         Update disparity map in GUI.
@@ -129,68 +138,94 @@ class BMTuner(object):
         cv2.waitKey()
     def tune_pair(self, pair):
         """Tune a pair of images."""
+        self._save_bm_state()
         self.pair = pair
         self.update_disparity_map()
+    def report_settings(self, parameter):
+        """
+        Report chosen settings for ``parameter`` in ``block_matcher``.
+
+        ``bm_settings`` is updated to include the latest value before work is
+        begun. This value is removed at the end so that the method has no side
+        effects. All settings are reported except for the first one on record,
+        which is the ``BlockMatcher``'s default setting.
+        """
+        self._save_bm_state()
+
+        report = []
+        settings_list = self.bm_settings[parameter]
+        unique_values = list(set(settings_list))
+        value_frequency = {}
+        for value in unique_values:
+            value_frequency[settings_list.count(value)] = value
+        frequencies = value_frequency.keys()
+        frequencies.sort(reverse=True)
+        header = "{} value | Selection frequency".format(parameter)
+        left_column_width = len(header[:-21])
+        right_column_width = 21
+        report.append(header)
+        report.append("{}|{}".format("-" * left_column_width,
+                                    "-" * right_column_width))
+        for frequency in frequencies:
+            left_column = str(value_frequency[frequency]).center(
+                                                             left_column_width)
+            right_column = str(frequency).center(right_column_width)
+            report.append("{}|{}".format(left_column, right_column))
+
+        # Remove settings
+        for param in self.block_matcher.parameter_maxima.keys():
+            self.bm_settings[param].pop(-1)
+        return "\n".join(report + ["\n"])
 
 def main():
     """
     Let user tune all images in the input folder and report chosen values.
 
-    Load all images from input folder, consuming available files and showing
-    them in the GUI. Afterwards, report user's chosen settings and, if a file
-    for the BM settings is provided, save the most common settings to file.
+    Load a calibration from file and instantiate a ``BlockMatcher`` of the type
+    requested by the user. Load images successively from input folder and
+    display their resultant disparity map generated with the ``BlockMatcher``
+    and the parameters chosen in the ``BMTuner``'s GUI. Afterwards, report
+    user's chosen settings and, if a file for the BM settings is provided, save
+    the most common settings to file.
     """
-    parser = argparse.ArgumentParser(description="Read images taken from a "
-                                     "calibrated stereo pair, compute "
-                                     "disparity maps from them and show them "
-                                     "interactively to the user, allowing the "
-                                     "user to tune the stereo block matcher "
-                                     "settings in the GUI.")
+    parser = ArgumentParser(description="Read images taken from a calibrated "
+                           "stereo pair, compute disparity maps from them and "
+                           "show them interactively to the user, allowing the "
+                           "user to tune the stereo block matcher settings in "
+                           "the GUI.")
     parser.add_argument("calibration_folder",
                         help="Directory where calibration files for the stereo "
                         "pair are stored.")
     parser.add_argument("image_folder",
                         help="Directory where input images are stored.")
     parser.add_argument("--bm_settings",
-                        help="File to save most commonly chosen block matcher "
-                        "settings to.", default="")
+                        help="File to save last block matcher settings to.",
+                        default="")
+    parser.add_argument("--use_sgbm", help="Use StereoSGBM rather than StereoBM "
+                        "block matcher.", action="store_true")
     args = parser.parse_args()
 
-    calibration = calibrate_stereo.StereoCalibration(
-                                        input_folder=args.calibration_folder)
+    calibration = StereoCalibration(input_folder=args.calibration_folder)
     input_files = find_files(args.image_folder)
-    calibrated_pair = CalibratedPair(None, calibration)
+    if args.use_sgbm:
+        block_matcher = StereoSGBM()
+    else:
+        block_matcher = StereoBM()
     image_pair = [cv2.imread(image) for image in input_files[:2]]
     rectified_pair = calibration.rectify(image_pair)
-    tuner = StereoBMTuner(calibrated_pair, rectified_pair)
-    chosen_arguments = []
+    tuner = BMTuner(block_matcher, calibration, rectified_pair)
+
     while input_files:
         image_pair = [cv2.imread(image) for image in input_files[:2]]
         rectified_pair = calibration.rectify(image_pair)
         tuner.tune_pair(rectified_pair)
-        chosen_arguments.append((calibrated_pair.stereo_bm_preset,
-                                 calibrated_pair.search_range,
-                                 calibrated_pair.window_size))
         input_files = input_files[2:]
-    stereo_bm_presets, search_ranges, window_sizes = [], [], []
-    for preset, search_range, size in chosen_arguments:
-        stereo_bm_presets.append(preset)
-        search_ranges.append(search_range)
-        window_sizes.append(size)
-    pretty_settings_names = {"stereo_bm_preset": "Stereo BM presets",
-                             "search_range": "Search ranges",
-                             "window_size": "Window sizes"}
-    common_settings = {}
-    for variable, values in (("stereo_bm_preset", stereo_bm_presets),
-                         ("search_range", search_ranges),
-                         ("window_size", window_sizes)):
-        (common_setting,
-         report) = report_variable(pretty_settings_names[variable], values)
-        common_settings[variable] = common_setting
-        print(report)
+
+    for param in block_matcher.parameter_maxima:
+        print(tuner.report_settings(param))
+
     if args.bm_settings:
-        with open(args.bm_settings, "w") as settings_file:
-            simplejson.dump(common_settings, settings_file)
+        block_matcher.save_settings(args.bm_settings)
 
 if __name__ == "__main__":
     main()
