ac5e534659ddc785ab5da72a3b07cd54b8734284
Johannes Mikulasch <johannes.mikulasch@zmp.co.jp>
2016-07-06 16:03:10 +0900

    support single stream cameras
    With single stream camera I mean that both the images of both the
    left and right camera are streamed side-by-side in a single image.

--- stereovision/stereo_cameras.py
+++ stereovision/stereo_cameras.py
@@ -54,8 +54,14 @@ class StereoPair(object):
 
         ``devices`` is an iterable containing the device numbers.
         """
-        #: Video captures associated with the ``StereoPair``
-        self.captures = [cv2.VideoCapture(device) for device in devices]
+
+        if devices[0] != devices[1]:
+            #: Video captures associated with the ``StereoPair``
+            self.captures = [cv2.VideoCapture(device) for device in devices]
+        else:
+            # Stereo images come from a single device, as single image
+            self.captures = [cv2.VideoCapture(devices[0])]
+            self.get_frames = self.get_frames_singleimage
 
     def __enter__(self):
         return self
@@ -69,6 +75,17 @@ class StereoPair(object):
     def get_frames(self):
         """Get current frames from cameras."""
         return [capture.read()[1] for capture in self.captures]
+
+    def get_frames_singleimage(self):
+        """
+        Get current left and right frames from a single image,
+        by splitting the image in half.
+        """
+        frame = self.captures[0].read()[1]
+        height, width, colors = frame.shape
+        left_frame = frame[:, :width/2, :]
+        right_frame = frame[:, width/2:, :]
+        return [left_frame, right_frame]
 
     def show_frames(self, wait=0):
         """
